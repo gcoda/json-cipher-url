5675674bf76a7141365249bc0ff4ef5562c7f6e9
gcoda <gcoding@gmail.com>
2018-12-22 22:13:44 +0200

    use hash from dep

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const BASE62 = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 const { encode, decode } = require('base-x')(BASE62)
 const jsonCipher = require('json-cipher')
+
 module.exports = (secret, algorithm = 'aes-256-cbc') => {
-  const { cipher, decipher } = jsonCipher(secret, algorithm)
+  const { cipher, decipher, hmac } = jsonCipher(secret, algorithm)
   return {
     stringify: object => encode(cipher(object)),
     parse: string => decipher(decode(string)),
-    hash(string, length = 32) {
-      const hash = x => {
-        const hashed = cipher(x)
-          .toString('hex')
-          
-        if (hashed.length < length) return hash(hashed)
-        else return hashed.substring(0, length)
-      }
-      return hash(string)
-    },
+    cipher,
+    decipher,
+    hmac,
+    hash: hmac,
   }
 }
